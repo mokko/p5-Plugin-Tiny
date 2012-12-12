cd72aff74c280e65aa5339da24c343670b260c94
mokko <mauricemengel@gmail.com>
2012-12-12 14:36:34 -0500

    inital commit

---
+++ lib/Plugin/Tiny.pm
@@ -0,0 +1,167 @@
+#ABSTRACT: A tiny plugin system for perl
+package Plugin::Tiny;
+use strict;
+use warnings;
+use Carp 'confess';
+use Class::Load 'load_class';
+use Moose;
+use namespace::autoclean;
+
+=head1 SYNOPSIS
+
+  #from core or bundling plugin
+  use Moose; #optional, t/tiny.t avoids using moose in core
+  use Plugin::Tiny; 
+  has 'plugins'=>(
+    is=>'ro',
+    isa=>'Plugin::Tiny', 
+    default=>sub{Plugin::Tiny->new()}
+  );
+  
+  #load plugin_class (and perhaps phase) from your configuration
+  $self->plugins->register(
+    phase=>$phase,         #optional; defaults to last part of plugin class
+    plugin=>$plugin_class, #required
+    role=>$role,           #optional
+    arg1=>$arg1,           #optional
+    arg2=>$arg2,           #optional
+  );
+
+  #execute your plugin's methods 
+  my $plugin=$self->get_plugin ($phase); 
+  $plugin->do_something(@args);  
+
+=head1 DESCRIPTION
+
+Plugin::Tiny has a lot less code than Plugin::Simple with almost the same 
+functionality.
+
+A limitation of Plugin::Tiny is that each phase can have only one plugin. 
+But you can still create bundles of plugins if you hand the plugin system down 
+to the (bundeling) plugin. That way, you can load multiple plugins for one 
+phase; you still need distinct phase labels for each plugin.
+
+  #in your core
+  $self->plugins->register(
+    phase=>'Scan', 
+    plugin=>'Plugin::ScanBundle', 
+    plugins=>$self->plugins, #plugin system
+  );
+
+  #in Plugin::ScanBundle
+  has 'plugins'=>(is=>'ro', isa=>'Plugin::Tiny', required=>1); 
+  $self->plugins->register (plugin=>'Plugin::Scan1'); 
+  $self->plugins->register (plugin=>'Plugin::Scan2'); 
+  
+  my $scan1=$self->plugins->get('Scan1');
+  $scan1->do_something(@args);  
+
+You may want to standardize the methods in your plugins, e.g. by using a role. 
+Perhaps you always want to require an execute method instead of do_something.
+  
+=method $plugin_system->register(phase=>$phase, plugin=>$plugin_class);  
+
+Optionally, you can also specify a role which your plugin will have to be able 
+to apply. Remaining key value pairs are passed down to the plugin constructor: 
+
+  $plugin_system->register (
+    phase=>$phase, 
+    plugin=>$plugin,
+    role=>$role,
+    plugins=>$plugin_system,
+    args=>$more_args,
+  );
+
+A side-effect is that your plugin cannot use 'phase', 'plugin', 'role' as 
+named arguments.
+
+Returns the newly created plugin object on success. Confesses on error.
+
+=cut
+
+has '_registry' => (    #href with phases and plugin objects
+    is       => 'ro',
+    isa      => 'HashRef[Object]',
+    default  => sub { {} },
+    init_arg => undef,
+);
+
+=attr prefix
+
+Optional. You can have the prefix added to all plugin classes you register so
+save some typing and force plugins in your namespace:
+
+  #without prefix  
+  my $ps=Plugin::Tiny->new  
+  $ps->register(plugin='Your::App::Plugin::Example1');
+  $ps->register(plugin='Your::App::Plugin::Example2');
+
+  #with prefix  
+  my $ps=Plugin::Tiny->new (  prefix=>'Your::App::Plugin::' );  
+  $ps->register(plugin='Example1');
+  $ps->register(plugin='Example2');
+
+=cut
+
+has 'prefix' => (is => 'ro', isa => 'Str');
+
+=attr role
+
+A default role to be applied to all plugins. Can be overwritten in register.
+
+=cut
+
+has 'role' => (is => 'ro', isa => 'Str');
+
+
+#
+# METHODS
+#
+
+sub register {
+    my $self   = shift;
+    my %args   = @_;
+    my $plugin = delete $args{plugin} or confess "Need plugin";
+    if ($self->prefix) {
+        $plugin = $self->prefix . $plugin;
+    }
+    my $phase = $args{phase} ? delete $args{phase} : _lastPart($plugin);
+
+    my $role = $self->role if $self->role;    #default role
+    $role = delete $args{role} if $args{role};
+
+    load_class($plugin) or confess "Can't load $plugin";
+    $self->{_registry}{$phase} = $plugin->new(%args);
+
+    if ($role && !$plugin->does($role)) {
+        confess qq(Plugin doesn't plugin into role '$role');
+    }
+    return $self->{_registry}{$phase};
+}
+
+
+=method my $plugin=$self->get_plugin ($phase);
+
+Returns the plugin object associated with the phase.
+
+=cut
+
+sub get_plugin {
+    return $_[0]->{_registry}{$_[1]};
+}
+
+
+#
+# PRIVATE
+#
+
+sub _lastPart {    #function!
+    my $plugin = shift;                 #a class name
+    my @parts = split('::', $plugin);
+    return $parts[-1];
+}
+
+
+__PACKAGE__->meta->make_immutable;
+
+1;
---
+++ t/tiny.t
@@ -0,0 +1,85 @@
+#!perl
+
+use strict;
+use warnings;
+use Test::More;
+use Plugin::Tiny;
+use Try::Tiny;
+use FindBin;
+use File::Spec;
+use Scalar::Util 'blessed';
+use Data::Dumper;
+use lib File::Spec->catfile('t', 'lib');
+
+use_ok('Plugin::Tiny');
+
+package SampleCore;
+use Moose;
+has 'plugin_system'=> (is=>'ro', isa=>'Plugin::Tiny', required=>1);
+1;
+
+package SampleBundle;
+use Moose;
+has 'core'=> (is=>'ro', isa=>'Object', required=>1);
+1;
+
+
+package main;
+
+
+
+my $ps = Plugin::Tiny->new();
+ok($ps, 'new');
+
+ok( $ps->register(
+        phase   => 'foo',               #required
+        plugin  => 'TinyTestPlugin',    #required
+        plugin_system => $ps,
+        bar     => 'tiny',
+    ),
+    'simple register'
+);
+
+try {
+    $ps->register(
+        phase  => 'foo',                #required
+        plugin => 'TinyTestPlugin',     #required
+        bar    => 'tiny',
+    );
+}
+finally {
+    ok(@_, 'register fails without attr plugin_system');
+};
+
+
+try {
+    $ps->register(
+        phase  => 'foo',                  #required
+        plugin => 'nonexistingPlugin',    #required
+        bar    => 'tiny',
+    );
+}
+finally {
+    ok(@_, 'register fails when non-existing plugin is required');
+};
+
+
+my ($p1, $p2);
+ok($p1 = $ps->get_plugin('foo'), 'get p1');
+is($p1->do_something, 'doing something', 'execute return value');
+ok($p1->register_another_plugin, 'registering a new plug from inside a plug');
+ok($p2 = $ps->get_plugin('bar'), 'get p2');
+is( $p2->do_something,
+    'a plugin that is loaded by another plugin',
+    'return looks good'
+);
+
+my $aCore=SampleCore->new(plugin_system=>$ps);
+ok ($aCore, 'aCore created');
+ok ($aCore->plugin_system->register(plugin=>'TinySubPlug', plugin_system=>$ps),'another register');
+ok ($aCore->plugin_system->register(plugin=>'TinyTestPlugin', plugin_system=>$ps),'another register');
+
+
+my $aCore=HandoverCore->new(core=>$aCore);
+
+done_testing;
